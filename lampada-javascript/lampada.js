d31bd17ca20136ad36651409d9f6565c56c60277
Pedro <91342788+httpxdro@users.noreply.github.com>
2022-08-06 07:26:05 -0300

    Add files via upload

---
+++ lampada-javascript/lampada.js
@@ -0,0 +1,31 @@
+const lamp = document.getElementById('lamp');
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+
+function isBroken() {
+    return lamp.src.indexOf ('quebrada') > -1
+}
+
+function lampOn() {
+    if (!isBroken()){
+    lamp.src = './img/ligada.jpg';
+}}
+
+function lampOff() {
+    if (!isBroken()){
+    lamp.src = './img/desligada.jpg'
+}}
+
+function lampBroken() {
+    lamp.src = './img/quebrada.jpg'
+}
+
+turnOn.addEventListener('click', lampOn);
+
+turnOff.addEventListener('click', lampOff);
+
+lamp.addEventListener('mouseover', lampOn);
+
+lamp.addEventListener('mouseleave', lampOff);
+
+lamp.addEventListener('dblclick', lampBroken);
